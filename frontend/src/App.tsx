d3036a2ebc83ecb662d9d829eed11fb7cb5a92b2
Junaid Shaikh <junaidshaikh3967@gmail.com>
2026-01-12 17:50:50 +0530

    chore(UI): install and setup shadcn/ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
       <h1>GIG FLOW INIT</h1>
+      <Button>TEST</Button>
     </>
   )
 }
