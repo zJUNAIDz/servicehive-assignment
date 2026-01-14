ed12647b2627415942650c7e7511042e6ad7fb78
Junaid Shaikh <junaidshaikh3967@gmail.com>
2026-01-15 04:05:59 +0530

    fix: type `any` build error suppression

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -1,4 +1,5 @@
-export function transformDoc(doc, ret) {
+// eslint-disable @typescript-eslint/no-explicit-any
+export function transformDoc(doc: any, ret: any): any {
   ret.id = ret._id; // Create id
   delete ret._id; // Delete _id
   delete ret.__v; // Delete version key
